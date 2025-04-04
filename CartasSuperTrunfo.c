f5c0d56078b0092a7b412d8352965dcf7fca2cae
projetochristian <christiandebrito@gmail.com>
2025-04-04 19:34:14 +0000

    crição da variavel

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -18,5 +18,39 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
+
+    char estado1;                  // ESTADO A.
+    char codigo1[20];              // CÓDIGO DA CARTA.
+    char cidade1[20];               // CIDADE BELÉM.
+    int populacao1;                // POPULAÇÃO. 
+    float area1;                   // ÁREA.
+    float pib1;                    // PIB.
+    int pontosturisticos1;          // PONTOS TURISTICOS. 
+
+
+    estado1 ='A';
+    codigo1[20] = "A1";
+    cidade1[20] ="belem";
+    populacao1 = 8600000;
+    area1 = 1245000000;
+    pib1 = 262000;
+    pontosturisticos1 = 3;
+
+
+
+
+    printf("Carta 1:\n");                        //IMPRIMIR CARTA
+    printf("Estado: %c\n", estado1);               //IMPRIMIR ESTADO 
+
+    printf("Código: %c\n\n\n", codigo1);                
+
+
+    
+
+
+
+
+
+
     return 0;
 }
